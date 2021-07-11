d70b68670f831a86bd2015646912596b85fcee1c
valdir-alves3000 <51173956+valdir-alves3000@users.noreply.github.com>
2021-07-11 10:59:05 -0300

    :heavy_plus_sign: Adding  dependencies and TypeORM config

---
+++ ormconfig.js
@@ -0,0 +1,14 @@
+module.exports = {
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+
+  // for local testing change the 'dist' folder to 'src' and the 'js' extension for 'ts'
+  
+  migrations: [`dist/database/migrations/*.js`],
+
+  entities: [`dist/entities/*.js`],
+  
+  cli: {
+    migrationsDir: 'src/database/migrations'
+  },
+}
