30234218b22bc2b1f2526e61062939f29249c21e
valdir-alves3000 <51173956+valdir-alves3000@users.noreply.github.com>
2021-07-13 17:40:44 -0300

    :heavy_plus_sign: Adding Update Products Service

---
+++ src/services/products/UpdateProductService.ts
@@ -0,0 +1,72 @@
+import { getCustomRepository } from 'typeorm';
+import { ProductsRepository } from '../../repositories';
+import { UsersRepository } from '../../repositories';
+
+import { Products } from '../../entities';
+
+interface IUserRequest {
+  id: string;
+  material: string;
+  status: string;
+  locked: boolean;
+  quantity: number;
+  description: string;
+  storage_location: string;
+  user_id: string;
+}
+
+class UpdateProductService {
+  async execute({
+    id,
+    material,
+    description,
+    storage_location,
+    quantity,
+    status,
+    locked,
+    user_id
+  }: IUserRequest) {
+    const productsRepository = getCustomRepository(ProductsRepository);
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const productAlreadyExists = await productsRepository.findOne(id);
+    const { admin } = await usersRepository.findOne(user_id);
+
+    if (!productAlreadyExists) {
+      throw new Error('Product not found!');
+    }
+
+    if (!admin) {
+      await productsRepository
+        .createQueryBuilder()
+        .update(Products)
+        .set({
+          storage_location: storage_location || productAlreadyExists.storage_location,
+          quantity: quantity || productAlreadyExists.quantity,
+          status: status || productAlreadyExists.status,
+          updated_by_user: user_id
+        })
+        .where('id = :id', { id })
+        .execute();
+
+      return;
+    }
+
+    await productsRepository
+      .createQueryBuilder()
+      .update(Products)
+      .set({
+        material: material || productAlreadyExists.material,
+        description: description || productAlreadyExists.description,
+        storage_location: storage_location || productAlreadyExists.storage_location,
+        quantity: quantity || productAlreadyExists.quantity,
+        status: status || productAlreadyExists.status,
+        locked: locked || productAlreadyExists.locked,
+        updated_by_user: user_id
+      })
+      .where('id = :id', { id })
+      .execute();
+  }
+}
+
+export { UpdateProductService };
--- src/services/products/index.ts
+++ src/services/products/index.ts
@@ -1,2 +1,3 @@
-export { CreateProductsService } from './CreateProductsService';
 export { ListProductsService } from './ListProductsService';
+export { UpdateProductService } from './UpdateProductService';
+export { CreateProductsService } from './CreateProductsService';
