bb2b05ad244df4fb8e3ff5da4e120bc1cf742747
valdir-alves3000 <51173956+valdir-alves3000@users.noreply.github.com>
2021-07-13 18:04:28 -0300

    :heavy_plus_sign: Adding Creating Storage Points Table

---
+++ src/database/migrations/1626209917959-CreateStoragePoints.ts
@@ -0,0 +1,57 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateStoragePoints1626209917959 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'storage_points',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true
+                    },
+                    {
+                        name: 'storage_point',
+                        type: 'varchar'
+                    },
+                    {
+                        name: 'locked',
+                        type: 'boolean'
+                    },
+                    {
+                        name: 'product_id',
+                        type: 'uuid'
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()'
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()'
+                    },
+
+                ],
+                foreignKeys: [
+                    {
+                        name: 'FKStoragePointProduct',
+                        referencedTableName: 'products',
+                        referencedColumnNames: ['id'],
+                        columnNames: ['product_id'],
+                        onDelete: 'SET NULL',
+                        onUpdate: 'SET NULL',
+                    },
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('storage_points');
+    }
+
+}
